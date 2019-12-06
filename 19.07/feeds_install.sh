2fa530458c8d2612b752a71106a3087350613115
August-Icekimo <august.icekimo@gmail.com>
2019-12-06 13:49:04 +0800

    在Jenkins的片段

---
+++ 19.07/feeds_install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ # Update source code and config
+ ./scripts/feeds update -a &&\
+ ./scripts/feeds install shadow sudo bash rsync nginx openssh tmux vim\
+ git git-gitweb gitolite fossil autossh fwknop fwknopd python3 php7 python screen\
+ isc-dhcp socat squid acme clamav ddns-scripts luci luci-app-fwknopd luci-app-ddns \
+ luci-app-commands luci-app-qos qos-scripts luci-app-squid luci-mod-admin-full \
+ luci-app-wol luci-app-acme acme-dnsapi libpam libssh2 libgnutls libidn2
