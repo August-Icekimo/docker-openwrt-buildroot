989891bde58a7fd8015315b05fa5332cd97f1a33
August-Icekimo <august.icekimo@gmail.com>
2019-12-18 13:36:04 +0800

    update feeds

--- 19.07/feeds_install.sh
+++ 19.07/feeds_install.sh
@@ -5,4 +5,5 @@
  git git-gitweb gitolite fossil autossh fwknop fwknopd python3 php7 python screen\
  isc-dhcp socat squid acme clamav ddns-scripts luci luci-app-fwknopd luci-app-ddns \
  luci-app-commands luci-app-qos qos-scripts luci-app-squid luci-mod-admin-full \
- luci-app-wol luci-app-acme acme-dnsapi libpam libssh2 libgnutls libidn2
+ luci-theme-material luci-app-wol luci-app-upnp  luci-app-diag-core luci-app-hd-idle \
+ luci-app-samba  luci-app-wol luci-app-acme acme-dnsapi libpam libssh2 libgnutls libidn2
