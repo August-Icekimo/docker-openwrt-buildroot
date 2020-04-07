a8e277524fcd70e9fd2a87a1cdf7a717d6e9a092
August-Icekimo <august.icekimo@gmail.com>
2020-04-07 15:35:11 +0800

    增加設定與編譯

--- 19.07/feeds_install.sh
+++ 19.07/feeds_install.sh
@@ -2,8 +2,17 @@
  # Update source code and config
  ./scripts/feeds update -a &&\
  ./scripts/feeds install shadow sudo bash rsync nginx openssh tmux vim\
- git git-gitweb gitolite fossil autossh fwknop fwknopd python3 php7 python screen\
- isc-dhcp socat squid acme clamav ddns-scripts luci luci-app-fwknopd luci-app-ddns \
- luci-app-commands luci-app-qos qos-scripts luci-app-squid luci-mod-admin-full \
- luci-theme-material luci-app-wol luci-app-upnp  luci-app-diag-core luci-app-hd-idle \
- luci-app-samba  luci-app-wol luci-app-acme acme-dnsapi libpam libssh2 libgnutls libidn2
+ git git-gitweb gitolite autossh python3 php7 python screen kmod-fs-exfat modemmanager\
+ isc-dhcp socat acme clamav luci fwknop fwknopd luci-app-fwknopd ddns-scripts luci-app-ddns \
+ luci-app-commands luci-app-sqm sqm-scripts jansson squid luci-app-squid luci-mod-admin-full \
+ luci-app-wol libpam libssh2 libgnutls libidn2 aria2 ariang-nginx luci-app-aria2 libcap
+
+# 下載設定檔
+# wget https://raw.githubusercontent.com/August-Icekimo/docker-openwrt-buildroot/master/19.07/archer-c7-v5.config -O .config && make download
+
+# 下載特殊包
+git clone https://github.com/BROBIRD/openwrt-r8168.git ./openwrt/package/kernel/openwrt-r8168
+
+# 編譯指令
+make clean
+make -j5 # V=s
